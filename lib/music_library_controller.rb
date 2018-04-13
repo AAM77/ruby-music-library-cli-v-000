2b84e37cbb7dcd5650628406fcf6d34d8e4a719e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-13 22:59:46 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
 
   def initialize(path = "./db/mp3s")
     @path = path
+<<<<<<< HEAD
     MusicImporter.new(path).import
   end # initialize
 
@@ -43,11 +44,35 @@ class MusicLibraryController
         play_song
       end # cases
     end # while
+=======
+    @imported = MusicImporter.new(path).import
+    @songs = @imported.collect {|song| song.chomp('.mp3')}
+  end # initialize
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    "Please enter input:"
+    user_input = gets.strip
+
+    until user_input == 'exit'
+      user_input = gets.strip
+    end # until
+
+>>>>>>> 951bdc0fb2ebd62a51c6954a4a7954af02a80e85
   end # call
 
 
 
   def list_songs
+<<<<<<< HEAD
     songs = Song.all.sort_by{|song| song.name}
     songs.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
@@ -106,3 +131,136 @@ class MusicLibraryController
   end # play_song
 
 end # class MusicLibraryController
+=======
+    sorted = @songs.sort_by{|filename| filename.split(' - ')[1]}
+    sorted.each do |song|
+      puts "#{1}. #{song}"
+    end # .each
+
+  end # list songs
+
+  def list_artists
+    artists = Artist.all.collect{|artist| artist.name}.sort.uniq
+    artists.each do |artist|
+      puts "#{artist.index + 1}. #{artist}"
+    end # .each
+  end # list artists
+
+
+  def list_genres
+    count = 1
+    genres = Genre.all.collect{|genre| genre.name}.sort.uniq
+    genres.each do |genre|
+      puts "#{count}. #{genre}"
+      count += 1
+    end # .each
+  end # list genres
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    user_artist = gets.chomp
+
+    while user_artist == nil
+      puts "Please enter the name of an artist:"
+      user_artist = gets.strip
+    end
+
+    list_songs.select do |song|
+      if song.split(' - ')[0] == user_artist
+        song
+      end # if
+    end # do
+  end # list songs by artist
+
+
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_genre = gets.strip
+
+    while user_genre == nil
+      puts "Please enter the name of a genre:"
+      user_genre = gets.strip
+    end
+
+    list_songs.select do |song|
+      if song.split(' - ')[2] == user_genre
+        song
+      end # if
+    end # do
+
+    ###
+  end # list songs by genre
+
+
+  end # class Music Library Controller
+
+=begin ###########################
+  def list_songs
+    count = 1
+    sorted = @imported.sort_by{|filename| filename.split(' - ')[1]}
+    songs = sorted.uniq
+    songs.each do |song|
+      puts "#{count}. #{song.gsub('.mp3','')}"
+      count += 1
+    end # .each
+
+  end # list songs
+
+  def list_artists
+    count = 1
+    artists = Artist.all.collect{|artist| artist.name}.sort.uniq
+    artists.each do |artist|
+      puts "#{count}. #{artist}"
+      count += 1
+    end # .each
+  end # list artists
+
+
+  def list_genres
+    count = 1
+    genres = Genre.all.collect{|genre| genre.name}.sort.uniq
+    genres.each do |genre|
+      puts "#{count}. #{genre}"
+      count += 1
+    end # .each
+  end # list genres
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    user_artist = gets.chomp
+
+    while user_artist == nil
+      puts "Please enter the name of an artist:"
+      user_artist = gets.strip
+    end
+
+    list_songs.select do |song|
+      if song.split(' - ')[0] == user_artist
+        song
+      end # if
+    end # do
+  end # list songs by artist
+
+
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_genre = gets.strip
+
+    while user_genre == nil
+      puts "Please enter the name of a genre:"
+      user_genre = gets.strip
+    end
+
+    list_songs.select do |song|
+      if song.split(' - ')[2] == user_genre
+        song
+      end # if
+    end # do
+
+    ###
+  end # list songs by genre
+
+=end ##############################
+>>>>>>> 951bdc0fb2ebd62a51c6954a4a7954af02a80e85
